ed582a9387b9d597fe2da3b4951462b42074de3c
reactnenja <reactnenja@gmail.com>
2024-04-30 19:20:48 +0100

    success

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Login from "./auth/Login";
 import Home from "./assets/pages/Home";
-import { AuthContext } from "./assets/context/SliceContext";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <AuthContext.Provider value={{ email: "", password: "" }}>
-          <Route path="/" element={<Login />} />
-        </AuthContext.Provider>
+        <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
